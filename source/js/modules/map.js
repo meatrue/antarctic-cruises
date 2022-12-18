40c9a4afc719e5045c1c709ea9e504800e7d726d
Михаил Трухин <meatrue@gmail.com>
2022-12-18 21:28:32 +0300

    Исправляет баги

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,45 +1,9 @@
-const PIN_ICON_PATH = './img/svg/address-marker.svg';
-
+const MAP_NOJS_CLASS = 'contacts__map--nojs';
 const mapContainerElement = document.querySelector('#map');
-const mapImageElement = document.querySelector('#map img');
-
-
-const setMap = () => {
-  if (mapImageElement) {
-    mapImageElement.remove();
-  }
-
-  const center = [59.938430492248656, 30.32316041534418];
-
-  const myMap = new ymaps.Map(mapContainerElement, { // eslint-disable-line
-    center,
-    zoom: 15,
-  });
-
-  /* stylelint-disable-next-line */
-  const placemark = new ymaps.Placemark(center, {}, { // eslint-disable-line
-    iconLayout: 'default#image',
-    iconImageHref: PIN_ICON_PATH,
-    iconImageSize: [18, 22],
-    iconImageOffset: [-22, 24],
-  });
-
-  myMap.controls.remove('geolocationControl');
-  myMap.controls.remove('searchControl');
-  myMap.controls.remove('trafficControl');
-  myMap.controls.remove('typeSelector');
-  myMap.controls.remove('fullscreenControl');
-  myMap.controls.remove('zoomControl');
-  myMap.controls.remove('rulerControl');
-  myMap.behaviors.disable(['scrollZoom']);
-
-  myMap.geoObjects.add(placemark);
-};
-
 
 const initMap = () => {
   if (mapContainerElement) {
-    ymaps.ready(setMap); // eslint-disable-line
+    mapContainerElement.classList.remove(MAP_NOJS_CLASS);
   }
 };
 
